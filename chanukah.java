0e1ab0645f9227e5b7983f323d992410b24535b8
psaicho <guzekt@gmail.com>
2022-10-10 09:47:48 +0200

    kolejne zadania

---
+++ chanukah.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class chanukah {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i=1;  i <= N; i++){
+            sc.nextInt();
+            int b = sc.nextInt();
+            int wynik = ((b+2)*(b+1)) /2 - 1;
+            System.out.println(i+ " " +wynik);
+        }
+    }
+}
