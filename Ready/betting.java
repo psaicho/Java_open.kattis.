5effc5cb850fd5572e0d2bbf39cfdf80361d888c
psaicho <guzekt@gmail.com>
2022-10-12 11:11:08 +0200

    Done >

---
+++ Ready/betting.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class betting{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    float l = sc.nextInt();
+    System.out.printf("%.10f %n %.10f",100/l, 100/(100-l));
+    }
+}
